ea57c4c272627ede2b731274ae48c366d484b486
Piyush Bhati <114479451+Piyush0bhati@users.noreply.github.com>
2026-07-05 18:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Person {
     }
 
     greet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 }
 
@@ -18,12 +18,9 @@ class Employee extends Person {
     }
 
     jobGreet() {
-        console.log(
-            `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-        );
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
-
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
